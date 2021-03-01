9a28e6f789b080879e3678dbb7601f732a9e6233
Russ Green <russ.green@live.co.uk>
2021-03-01 08:01:05 +0000

    wip

--- TopoAlign/App.cs
+++ TopoAlign/App.cs
@@ -10,16 +10,85 @@ using System.Collections.Generic;
 
 namespace TopoAlign
 {
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
     class App : IExternalApplication
     {
+        public static UIControlledApplication cachedUiCtrApp;
+
+        public static Autodesk.Revit.DB.Document revitDocument;
+
         public Result OnStartup(UIControlledApplication a)
         {
+            cachedUiCtrApp = a;
+            var ribbonPanel = CreateRibbonPanel();
+
             return Result.Succeeded;
         }
 
         public Result OnShutdown(UIControlledApplication a)
         {
             return Result.Succeeded;
+        }
+
+        private RibbonPanel CreateRibbonPanel()
+        {
+            RibbonPanel panel;
+
+            // Check if "Archisoft Tools already exists and use if its there
+            try
+            {
+                panel = cachedUiCtrApp.CreateRibbonPanel("Archisoft Tools", Guid.NewGuid().ToString());
+                panel.Name = "ARBG_Transmittal_ExtApp";
+                panel.Title = "eProject Transmittal";
+            }
+            catch
+            {
+                var archisoftPanel = false;
+                var pluginPath = @"C:\ProgramData\Autodesk\ApplicationPlugins";
+                if (System.IO.Directory.Exists(pluginPath) == true)
+                {
+                    foreach (var folder in System.IO.Directory.GetDirectories(pluginPath))
+                    {
+                        if(folder.ToLower().Contains("archisoft") == true & folder.ToLower().Contains("archisoft topoalign") == false)
+                        {
+                            archisoftPanel = true;
+                            break;
+                        }
+                    }
+
+                    if(archisoftPanel == true)
+                    {
+                        cachedUiCtrApp.CreateRibbonTab("Archisoft Tools");
+                        panel = cachedUiCtrApp.CreateRibbonPanel("Archisoft Tools", Guid.NewGuid().ToString());
+                        panel.Name = "ARBG_Transmittal_ExtApp";
+                        panel.Title = "eProject Transmittal";
+                    }
+                    else
+                    {
+                        panel = cachedUiCtrApp.CreateRibbonPanel("Topo Align");
+                    }
+                }
+
+
+            }
+
+            return panel;
+        }
+        private System.Windows.Media.ImageSource PngImageSource(string embeddedPath)
+        {
+            var stream = GetType().Assembly.GetManifestResourceStream(embeddedPath);
+            System.Windows.Media.ImageSource imageSource;
+            try
+            {
+                imageSource = BitmapFrame.Create(stream);
+            }
+            catch
+            {
+                imageSource = null;
+            }
+
+            return imageSource;
         }
     }
 }
