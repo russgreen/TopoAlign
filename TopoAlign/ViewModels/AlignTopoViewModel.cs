660f6c352d346b353e1207af168293596d60e188
Russ Green <1886088+russgreen@users.noreply.github.com>
2023-05-06 13:19:22 +0100

    basic setup of view layout

---
+++ TopoAlign/ViewModels/AlignTopoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Reflection;
+
+namespace TopoAlign.ViewModels;
+internal partial class AlignTopoViewModel : BaseViewModel
+{
+    public string WindowTitle { get; private set; }
+
+    [ObservableProperty]
+    private System.Windows.Visibility _isWindowVisible = System.Windows.Visibility.Visible;
+
+    public AlignTopoViewModel()
+    {
+        var informationVersion = Assembly.GetExecutingAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            .InformationalVersion;
+
+        WindowTitle = $"TopoAlign {informationVersion}";
+    }
+}
