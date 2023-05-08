cf0ba29949c9d0856ccebb4cf5feabde775759e6
Russ Green <1886088+russgreen@users.noreply.github.com>
2023-05-08 13:43:01 +0100

    Code cleanup

--- TopoAlign/Commands/CommandAlignTopo.cs
+++ TopoAlign/Commands/CommandAlignTopo.cs
@@ -1,60 +1,22 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
 
 namespace TopoAlign.Commands;
 
 [Transaction(TransactionMode.Manual)]
 public class CommandAlignTopo : IExternalCommand
 {
-    private UIApplication _uiapp;
-    private UIDocument _uidoc;
-    private Autodesk.Revit.ApplicationServices.Application _app;
-    private Document _doc;
-    private Selection _sel;
-    private decimal _offset;
-    private decimal _divide;
-    private Element _element;
-    private Edge _edge;
-
-#if REVIT2024_OR_GREATER
-    private Toposolid _topoSolid;
-#else
-    private Autodesk.Revit.DB.Architecture.TopographySurface _topoSurface;
-#endif
-
-    private Units _docUnits;
-
-#if REVIT2018 || REVIT2019 || REVIT2020
-    private DisplayUnitType _docDisplayUnits; 
-    private DisplayUnitType _useDisplayUnits;
-#else
-    private ForgeTypeId _docDisplayUnits;
-    private ForgeTypeId _useDisplayUnits;
-#endif
-
-    public Models.Settings cSettings;
-
-    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+     public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
-        cSettings = new Models.Settings();
-        cSettings.LoadSettings();
-
-        _uiapp = commandData.Application;
-        _uidoc = _uiapp.ActiveUIDocument;
-        _app = _uiapp.Application;
-        _doc = _uidoc.Document;
-        _sel = _uidoc.Selection;
+        App.CachedUiApp = commandData.Application;
+        App.RevitDocument = commandData.Application.ActiveUIDocument.Document;
 
         //check entitlement
-        if (CheckEntitlement.LicenseCheck(_app) == false)
+        if (CheckEntitlement.LicenseCheck(App.CachedUiApp.Application) == false)
         {
             return Result.Cancelled;
         }
-
-        App.CachedUiApp = commandData.Application;
-        App.RevitDocument = commandData.Application.ActiveUIDocument.Document;
 
         var newView = new Views.AlignTopoView();
         newView.ShowDialog();
