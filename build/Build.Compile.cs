c28cc950ffacf044c4cf856cfac3e8cc352b01d4
Russ Green <1886088+russgreen@users.noreply.github.com>
2024-03-28 07:25:01 +0000

    added build project

---
+++ build/Build.Compile.cs
@@ -0,0 +1,44 @@
+﻿using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Octokit;
+using Serilog;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+partial class Build
+{
+    Target Compile => _ => _
+    .DependsOn(Clean)
+    .Executes(() =>
+    {       
+        foreach (var configuration in GlobBuildConfigurations())
+        {
+            Log.Information("Configuration name: {configuration}", configuration);
+
+            if (configuration.StartsWith("Release"))
+            {
+                DotNetBuild(settings => settings
+                    .SetConfiguration(configuration)
+                    .SetVerbosity(DotNetVerbosity.minimal));
+            }
+        }
+
+    });
+
+    IEnumerable<string> GlobBuildConfigurations()
+    {
+        var configurations = Solution.Configurations
+            .Select(pair => pair.Key)
+            .Select(config => config.Remove(config.LastIndexOf('|')))
+            .ToList();
+            //.Where(config => Configurations.Any(wildcard => FileSystemName.MatchesSimpleExpression(wildcard, config)))
+            //.ToList();
+
+        //Assert.NotEmpty(configurations, $"No solution configurations have been found. Pattern: {string.Join(" | ", Configurations)}");
+        return configurations;
+    }
+}
---
+++ build/Build.Installer.cs
@@ -0,0 +1,21 @@
+﻿using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Octokit;
+using Serilog;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+partial class Build
+{
+    Target Installer => _ => _
+    .DependsOn(Sign)
+    .Executes(() =>
+    {
+
+    });
+}
