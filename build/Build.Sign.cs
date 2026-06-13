bfd343cad6c5f77c7f1129096f44469de9f4a8bc
Russ Green <1886088+russgreen@users.noreply.github.com>
2026-06-13 07:24:15 +0100

    migrated nuke to fallout

--- build/Build.Sign.cs
+++ build/Build.Sign.cs
@@ -1,11 +1,11 @@
-﻿using Nuke.Common;
-using Nuke.Common.Git;
-using Nuke.Common.IO;
-using Nuke.Common.Tools.SignTool;
+using Fallout.Common;
+using Fallout.Common.Git;
+using Fallout.Common.IO;
+using Fallout.Common.Tools.SignTool;
 using Serilog;
 using System.Collections.Generic;
 using System.Linq;
-using static Nuke.Common.Tools.SignTool.SignToolTasks;
+using static Fallout.Common.Tools.SignTool.SignToolTasks;
 
 partial class Build
 {
